6b3a6e18209c6fdb80d5efd781b61ae4bef202ad
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2020-04-23 23:48:13 -0500

    added get tools route and started on add tools

--- api/routes/tools/tools-model.js
+++ api/routes/tools/tools-model.js
@@ -2,9 +2,13 @@ const db  = require('../../../database/dbConfig')
 
 module.exports = {
     getTools,
-
+    getUserTools
 }
 
 function getTools(){
     return db('tools').where('available', 1)
 } 
+
+function getUserTools(user){
+    return db('tools').where('owner_id', user)
+}
