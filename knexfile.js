dd0ab70f30b9ae52b93093358eba033d682aa460
Matthew Meeves <matthew.meeves@gmail.com>
2021-03-21 13:28:49 -0500

    added dev2 environ for local work with production db

--- knexfile.js
+++ knexfile.js
@@ -2,64 +2,83 @@
 require("dotenv").config("./env");
 
 module.exports = {
-
   development: {
-    client: 'pg',
+    client: "pg",
     connection: {
-      host: 'localhost',
+      host: "localhost",
       port: 5432,
       user: process.env.USER,
       password: process.env.PASSWORD,
-      database: 'use-my-tools'
+      database: "use-my-tools",
     },
     migrations: {
-      directory: './database/migrations',
+      directory: "./database/migrations",
     },
     seeds: {
-      directory: './database/seeds',
+      directory: "./database/seeds",
     },
     pool: {
-      min: 2, 
+      min: 2,
       max: 10,
-    }
+    },
+  },
+
+  development2: {
+    client: "pg",
+    connection: {
+      host: "ec2-3-228-114-251.compute-1.amazonaws.com",
+      port: 5432,
+      user: process.env.USER2,
+      password: process.env.PASSWORD2,
+      database: "d65qk1h9u79pt2",
+    },
+    migrations: {
+      directory: "./database/migrations",
+    },
+    seeds: {
+      directory: "./database/seeds",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
   },
 
   staging: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "my_db",
+      user: "username",
+      password: "password",
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      directory: './database/migrations'
-    }
+      directory: "./database/migrations",
+    },
   },
-  
+
   testing: {
-    client: 'sqlite3',
-    connection: { filename: './database/testing.db3' },
+    client: "sqlite3",
+    connection: { filename: "./database/testing.db3" },
     useNullAsDefault: true,
     migrations: {
-      directory: './database/migrations',
-      tableName: 'dbmigrations',
+      directory: "./database/migrations",
+      tableName: "dbmigrations",
     },
-    seeds: { directory: './database/seeds' },
-},
+    seeds: { directory: "./database/seeds" },
+  },
   production: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      directory: './database/migrations',
+      directory: "./database/migrations",
     },
-
-  }
+  },
 };
